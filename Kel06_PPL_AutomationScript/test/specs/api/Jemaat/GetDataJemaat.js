7a15967788d8cafe7353b1d731303fb43d179207
raysiagian <anggasiagian17@gmail.com>
2023-12-12 23:12:34 +0700

    adding rpp, jemaat, and martumpol

---
+++ Kel06_PPL_AutomationScript/test/specs/api/Jemaat/GetDataJemaat.js
@@ -0,0 +1,53 @@
+const axios = require('axios');
+const { expect } = require('chai');
+
+describe('Get By ID rpp API Test', () => {
+  const id = 3;
+  const url = `http://127.0.0.1:8070/api/jemaat/${id}`; // Corrected URL with backticks for string interpolation
+
+  it('should return a 200 status code', async () => {
+    try {
+      const response = await axios.get(url);
+      expect(response.status).to.equal(200); // Check for a 200 status
+    } catch (error) {
+      throw new Error(`Failed to fetch data: ${error.message}`);
+    }
+  });
+
+  it('should return response in json content-type', async () => {
+    try {
+      const response = await axios.get(url);
+      const contentType = response.headers['content-type'];
+      expect(contentType.toLowerCase()).to.include('application/json'); // Check if it's JSON or not
+    } catch (error) {
+      throw new Error(`Failed to fetch todos: ${error.message}`);
+    }
+  });
+
+  it('should return the response property "code" with 200 value', async () => {
+    try {
+      const response = await axios.get(url);
+      expect(response.data).to.have.property('code').to.equal(200); // Check the value of the "code" property
+    } catch (error) {
+      throw new Error(`Failed to fetch todos: ${error.message}`);
+    }
+  });
+
+  it('should return the response property "message" with "Success" value', async () => {
+    try {
+      const response = await axios.get(url);
+      expect(response.data).to.have.property('message').to.equal('Success');
+    } catch (error) {
+      throw new Error(`Failed to fetch todos: ${error.message}`);
+    }
+  });
+
+  it('should return the response property "data" as an array', async () => {
+    try {
+      const response = await axios.get(url);
+      expect(response.data).to.have.property('data').that.is.an('array'); // Check if the "data" property is an array
+    } catch (error) {
+      throw new Error(`Failed to fetch todos: ${error.message}`);
+    }
+  });
+});
